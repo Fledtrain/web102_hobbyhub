7ca4b32b7af72cc6c055f9c639a9893f340c3d25
Fledtrain <Joeyew2014@gmail.com>
2023-11-05 01:16:27 -0400

    Updated Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,23 @@
+import { Link } from "react-router-dom"
 
 const Header = () => {
     return (
         <>
-            <h2>Header</h2>
+            <nav className="p-[44px] flex justify-around items-center hero-overlay">
+                <h2 className="text-4xl">The GamerHub</h2>
+                <ul className="space-x-6">
+                    <Link to="/">
+                        <button className="btn ">
+                            Home
+                        </button>
+                    </Link>
+                    <Link to="/create">
+                        <button className="btn ">
+                            Create new Post
+                        </button>
+                    </Link>
+                </ul>
+            </nav>
         </>
     )
 }
