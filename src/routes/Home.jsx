6fdd38e4d9ed82ed5464e5bef2acfb62572a8fd6
Joey <Joeyew2014@gmail.com>
2023-11-05 00:47:15 -0400

    Updated Home page to get posts

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react"
+import { supabase } from "../client"
+
 const Home = () => {
+    const [posts, setPosts] = useState([])
+
+    const getPosts = async () => {
+        let { data, error } = await supabase
+            .from('posts')
+            .select('*')
+
+        console.log(data, error)
+    }
+    useEffect(() => {
+        getPosts()
+    }, [])
+
     return (
         <>
             <h1>Home Page</h1>
