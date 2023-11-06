1e2513959aac5c0724e3e934ade6f6571551572c
Fledtrain <Joeyew2014@gmail.com>
2023-11-05 20:24:16 -0500

    Added Create Post feature

--- src/routes/Create.jsx
+++ src/routes/Create.jsx
@@ -1,7 +1,67 @@
+import { useState } from "react"
+import { supabase } from "../client"
+
 const Create = () => {
+    const [form, setForm] = useState({
+        title: '',
+        description: '',
+        passKey: 0,
+        image: ''
+    })
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+    
+
+        const createPost = async () => {
+            await supabase
+                .from('posts')
+                .insert([
+                    { title: form.title, content: form.description, secretKey: form.passKey, image: form.image }
+                ])
+        }
+
+        await createPost()
+    }
+
     return (
         <>
-            <div>Create</div>
+            <section className="p-[90px] ">
+                <form className="">
+                    <div className="">
+                        <input
+                            type="text"
+                            placeholder="Enter Title"
+                            className="input input-bordered mb-5"
+                            onChange={(e) => setForm(
+                                { ...form, title: e.target.value }
+                            )} />
+                    </div>
+                    <div className="form-control">
+                        <textarea
+                            className="textarea textarea-bordered h-24"
+                            placeholder="Enter Description"
+                            onChange={(e) => setForm({
+                                ...form, description: e.target.value
+                            })}
+                        ></textarea>
+                        <label className="label">
+                            <span className="label-text-alt"></span>
+                            <span className="label-text-alt">Enter Description </span>
+                        </label>
+                    </div>
+                    <div>
+                        <input
+                            type="number"
+                            placeholder="Enter Passkey"
+                            className="input input-bordered"
+                            onChange={(e) => setForm(
+                                { ...form, passKey: Number(e.target.value) }
+                            )} />
+                    </div>
+                    <button className="btn" onClick={handleSubmit}>Create Post</button>
+                </form>
+            </section>
         </>
     )
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -19,17 +19,29 @@ const Home = () => {
         getPosts()
     }, [])
 
+    console.log(posts[0]?.created_at)
+    // Format date to hours
+    const date = new Date(posts[0]?.created_at)
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+    const formattedDate = `${hours}:${minutes}:${seconds}`
+    console.log(formattedDate)
+
+
     return (
         <>
-            <section>
-                <h1>Home Page</h1>
-                {posts.map(post => (
-                    <div key={post.id}>
-                        <h2>{post.title}</h2>
-                        <p>{post.content}</p>
-                    </div>
+            <section className="p-[60px]">
+                {posts && posts.map(post => (
+                    <section key={post?.id} className="p-[40px] w-96 shadow-xl card card-bordered">
+                            <p>Posted {formattedDate} hours ago </p>
+                        <div className="card-body">
+                            <h2 className="card-title">{post?.title}</h2>
+                            <p className="card">{post?.content}</p>
+                        </div>
+                    </section>
                 ))}
-                </section>
+            </section>
         </>
     )
 }
