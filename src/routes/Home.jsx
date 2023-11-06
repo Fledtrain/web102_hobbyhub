4723893ef3bdfde7db959ca80735f9d9a01bb6d1
Fledtrain <Joeyew2014@gmail.com>
2023-11-06 00:50:06 -0500

    Added Redirect link to post page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { supabase } from "../client"
+import { Link } from "react-router-dom"
 
 const Home = () => {
     const [posts, setPosts] = useState([])
@@ -34,10 +35,15 @@ const Home = () => {
             <section className="p-[60px]">
                 {posts && posts.map(post => (
                     <section key={post?.id} className="p-[40px] w-96 shadow-xl card card-bordered">
-                            <p>Posted {post.created_at} hours ago </p>
+                        <p>Posted {post.created_at} hours ago </p>
                         <div className="card-body">
                             <h2 className="card-title">{post?.title}</h2>
                             <p className="card">{post?.content}</p>
+                        </div>
+                        <div className="card-footer">
+                            <Link to={`/post/${post?.postID}`}>
+                                <button className="btn ">Read More</button>
+                            </Link>
                         </div>
                     </section>
                 ))}
