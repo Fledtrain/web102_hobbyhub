b66128708778cc8e9e1cc17157572c2aa4812645
Fledtrain <Joeyew2014@gmail.com>
2023-11-09 16:20:24 -0500

    Added loading, error messages, and added UserID for identifying posts

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -6,19 +6,33 @@ import { data } from 'autoprefixer';
 const CommentSection = ({ postID }) => {
     const [comments, setComments] = useState([]);
     const [newComment, setNewComment] = useState('');
+    const [userID, setUserID] = useState('');
+    const [commentError, setCommentError] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const getComments = async () => {
         const { data } = await supabase
             .from('comments')
             .select()
-            .eq('postID', postID);
+            .eq('postID', postID)
+            .order('created_at', { ascending: false });
 
         if (!data) return;
         setComments(data);
     };
 
     const addComment = async () => {
-        if (newComment.trim() === '') return;
+        setLoading(true)
+        if (newComment.trim() === '') {
+            setCommentError(true)
+            setLoading(false)
+            return;
+        }
+        if (userID.trim() === '') {
+            setCommentError(true)
+            setLoading(false)
+            return;
+        }
 
         await supabase
             .from('comments')
@@ -26,15 +40,18 @@ const CommentSection = ({ postID }) => {
                 {
                     postID: postID,
                     comment: newComment,
+                    user_id: userID,
                 },
             ])
             .eq('postID', postID)
 
         // Clear the input field after adding a comment
         setNewComment('');
+        setUserID('');
 
         // Fetch comments again to include the newly added comment
         getComments();
+        setLoading(false)
     };
 
     useEffect(() => {
@@ -44,20 +61,33 @@ const CommentSection = ({ postID }) => {
     return (
         <section >
             <h2 className='hero text-4xl'>Comments Section</h2>
-            <aside className=''>
+            <div className='flex justify-center'>
+                {commentError && <p className='alert alert-error mt-2 w-[40rem] '>Please fill in the info before Commenting!</p>}
+            </div>
+            <form className=''>
                 <textarea
                     className='textarea textarea-bordered h-40 hero mt-5 '
                     value={newComment}
                     onChange={(e) => setNewComment(e.target.value)}
                     placeholder="Add a comment"
                 />
-            </aside>
+                <div className='hero'>
+                    <input
+                        type="number"
+                        className='input input-bordered w-[20rem] text-center mt-4'
+                        placeholder='Enter A ID'
+                        onChange={(e) => setUserID(Number(e.target.value))}
+                    />
+                </div>
+            </form>
             <div className='hero'>
-                <button onClick={addComment} className='btn mt-4  w-[20rem]'>Submit</button>
+                {loading ? <p className='loading'></p> :
+                    <button onClick={addComment} className='btn mt-4 w-[20rem]'>Submit</button>
+                }
             </div>
             <ul className=' hero-overlay mt-2'>
                 {comments?.map((comment) => (
-                    <li key={comment.postID}>User {1}: {comment.comment}</li>
+                    <li key={comment.id}>User {comment.user_id}: {comment.comment}</li>
                 ))}
             </ul>
         </section>
