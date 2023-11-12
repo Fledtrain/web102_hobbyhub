d3bb7f6f602d04e44a5a89432ede3e19462d6bd8
Fledtrain <Joeyew2014@gmail.com>
2023-11-11 20:14:26 -0500

    Refactored Home Route, Created AllPosts component

---
+++ src/components/AllPosts.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom"
+import { supabase } from "../client"
+
+
+const AllPosts = ({ posts, setPosts, status }) => {
+    const likeCounter = async (postID) => {
+        const postToUpdate = posts.find(post => post.id === postID)
+
+        if (postToUpdate) {
+            const updatedLikes = postToUpdate.likes + 1
+
+            await supabase
+                .from('posts')
+                .update({ likes: updatedLikes })
+                .eq('id', postID)
+                .select('likes')
+
+            setPosts(prevPosts => prevPosts.map(post => {
+                if (post.id === postID) {
+                    return { ...post, likes: updatedLikes }
+                }
+                return post
+            }))
+
+        }
+    }
+    return (
+        <>
+            <section className="p-[60px] grid xl:grid-cols-3 grid-cols-1 md:grid-cols-2">
+                {posts && posts.map(post => (
+                    <section key={post?.id} className="p-[40px] w-96 card-bordered card card-normal mt-4 bg-neutral-focus shadow-lg">
+                        {status.loading ?
+                            <div className="card-body">
+                                <p className="loading loading-lg card-title ml-[6rem]">1</p>
+                            </div>
+                            :
+                            <>
+                                <p>Posted {post.created_at} </p>
+                                <div className="card-body">
+                                    <h2 className="card-title">{post?.title}</h2>
+                                </div>
+                                <div className="card-footer flex space-x-32">
+                                    <Link to={`/post/${post?.postID}`}>
+                                        <button className="btn ">Read More</button>
+                                    </Link>
+                                    <button className="btn " onClick={() => likeCounter(post?.id)}>👍{post?.likes}</button>
+                                </div>
+                            </>
+                        }
+                    </section>
+                ))}
+            </section>
+        </>
+    )
+}
+
+export default AllPosts
