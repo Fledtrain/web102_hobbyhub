6a80cda0362534e1ddc5149da077cf08c0b14a63
Joey <Joeyew2014@gmail.com>
2023-11-14 11:57:06 -0500

    Added loading component and updated home route

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+/** Returns a loading spinner
+ * 
+ * @returns Loading
+ */
+const Loading = () => {
+    return (
+        <>
+            <p className="loading loading-lg"> </p>
+        </>
+    )
+}
+
+export default Loading
