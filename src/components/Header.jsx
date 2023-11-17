ac680f40fc295175277590bf0ebf5dc1f0c60adb
Fledtrain <Joeyew2014@gmail.com>
2023-11-16 22:03:31 -0500

    Fixed Header in mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,22 +7,22 @@ import { Link } from "react-router-dom"
 const Header = () => {
     return (
         <>
-            <nav className="p-[44px] flex justify-around items-center hero-overlay">
+            <nav className="p-[20px] md:p-[44px] flex justify-around items-center hero-overlay">
                 <Link to="/" aria-current="page">
                     <h2 className="text-4xl text-white">The Gamer<span className="">Hub</span></h2>
                 </Link>
-                <ul className="space-x-6">
+                <div className="hidden sm:flex  sm:space-x-8">
                     <Link to="/" aria-current='page'>
                         <button className="btn text-[#DDDDDD]">
                             Home
                         </button>
                     </Link>
                     <Link to="/create">
-                        <button className="btn text-[#DDDDDD] ">
+                        <button className=" btn text-[#DDDDDD] ">
                             Create new Post
                         </button>
                     </Link>
-                </ul>
+                </div>
             </nav>
         </>
     )
